82f8577fb3cda2ca6cd1e3e9d378f6edad9a97c4
liyanpeng5 <liyanpeng5@wanda.cn>
2015-08-28 21:53:41 +0800

    Add kclient processor template project.

---
+++ kclient-processor/src/main/java/com/robert/kclient/processor/KClientProcessor.java
@@ -0,0 +1,9 @@
+package com.robert.kclient.processor;
+
+public class KClientProcessor 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
