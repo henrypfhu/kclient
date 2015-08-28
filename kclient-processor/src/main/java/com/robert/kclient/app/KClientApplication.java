a34c9c928456f060525e2b056609854e65fcd3b4
liyanpeng5 <liyanpeng5@wanda.cn>
2015-08-28 23:41:42 +0800

    Add comments.

--- kclient-processor/src/main/java/com/robert/kclient/app/KClientApplication.java
+++ kclient-processor/src/main/java/com/robert/kclient/app/KClientApplication.java
@@ -1,15 +1,35 @@
 package com.robert.kclient.app;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * Spring boot main class which starts spring rest controller in the same
+ * package.
+ * 
+ * @author Robert Lee
+ * @since Aug 21, 2015
+ *
+ */
 @SpringBootApplication
 public class KClientApplication {
+	protected static Logger log = LoggerFactory
+			.getLogger(KClientApplication.class);
+
 	public static void main(String[] args) {
 		ApplicationContext ctxBackend = SpringApplication.run(
 				KClientApplication.class, args);
 
-		System.out.println("KClient application is started.");
+		String startupTime = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss z")
+				.format(new Date(ctxBackend.getStartupDate()));
+		log.info("KClient application starts at: " + startupTime);
+
+		System.out.println("KClient application starts at: " + startupTime);
 	}
 }
--- kclient-processor/src/main/java/com/robert/kclient/app/KClientController.java
+++ kclient-processor/src/main/java/com/robert/kclient/app/KClientController.java
@@ -1,5 +1,10 @@
 package com.robert.kclient.app;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.robert.kafka.kclient.boot.KClientBoot;
 
+/**
+ * Backend controller used to monitor and restart the KClient.
+ * 
+ * @author Robert Lee
+ * @since Aug 21, 2015
+ *
+ */
+
 @RestController
 public class KClientController {
+	protected static Logger log = LoggerFactory
+			.getLogger(KClientApplication.class);
 
 	private ApplicationContext ctxKafkaProcessor = new ClassPathXmlApplicationContext(
 			"kafka-application.xml");
@@ -26,11 +41,18 @@ public class KClientController {
 
 	@RequestMapping("/restart")
 	public String restart() {
+		log.info("Shutdowning KClient now...");
 		getKClientBoot().shutdownAll();
+
+		log.info("Restarting KClient now...");
 		ctxKafkaProcessor = new ClassPathXmlApplicationContext(
 				"kafka-application.xml");
 
-		return "KClient processor is restarted.";
+		String startupTime = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss z")
+				.format(new Date(ctxKafkaProcessor.getStartupDate()));
+		log.info("KClient application restarts at: " + startupTime);
+
+		return "KClient application restarts at: " + startupTime;
 	}
 
 	private KClientBoot getKClientBoot() {
