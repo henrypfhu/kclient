871d2ce7d20a761800d0561b95f114815f04ce10
liyanpeng5 <liyanpeng5@wanda.cn>
2015-08-30 13:19:37 +0800

    Tests passed.

--- kclient-processor/src/main/java/com/robert/kclient/app/handler/AnimalsHandler.java
+++ kclient-processor/src/main/java/com/robert/kclient/app/handler/AnimalsHandler.java
@@ -1,5 +1,8 @@
 package com.robert.kclient.app.handler;
 
+import java.io.IOException;
+
+import com.robert.kafka.kclient.boot.ErrorHandler;
 import com.robert.kafka.kclient.boot.InputConsumer;
 import com.robert.kafka.kclient.boot.KafkaHandlers;
 import com.robert.kafka.kclient.boot.OutputProducer;
@@ -18,13 +21,20 @@ public class AnimalsHandler {
 	@InputConsumer(propertiesFile = "kafka-consumer.properties", topic = "test", streamNum = 1)
 	@OutputProducer(propertiesFile = "kafka-producer.properties", defaultTopic = "test1")
 	public Cat dogHandler(Dog dog) {
-		System.out.println("Annotated dogHandler handle: " + dog);
+		System.out.println("Annotated dogHandler handles: " + dog);
 
 		return new Cat(dog);
 	}
 
 	@InputConsumer(propertiesFile = "kafka-consumer.properties", topic = "test1", streamNum = 1)
-	public void catHandler(Cat cat) {
-		System.out.println("Annotated catHandler handle: " + cat);
+	public void catHandler(Cat cat) throws IOException {
+		System.out.println("Annotated catHandler handles: " + cat);
+
+		throw new IOException("Man made exception.");
+	}
+
+	@ErrorHandler(exception = IOException.class, topic = "test1")
+	public void ioExceptionHandler(IOException e, String message) {
+		System.out.println("Annotated excepHandler handles: " + e);
 	}
 }
